b52d3da06e393ac33a17a0d05994920917adad20
billy <billyz404meter@gmail.com>
2023-11-11 21:49:04 -0800

    update settings

--- frontend/src/routes/Settings.js
+++ frontend/src/routes/Settings.js
@@ -9,6 +9,9 @@ function Settings() {
   const [homeAddress, setHomeAddress] = useState('')
   const [workAddress, setWorkAddress] = useState('')
   const [otherAddress, setOtherAddress] = useState('')
+  const [homeAddressZip, setHomeAddressZip] = useState('')
+  const [workAddressZip, setWorkAddressZip] = useState('')
+  const [otherAddressZip, setOtherAddressZip] = useState('')
 
   function handleSubmit(event) {
       event.preventDefault();
@@ -61,7 +64,9 @@ function Settings() {
                     required
                     sx={{mb: 4}}
                 />
-                <TextField
+                <Stack spacing="2px" style={{marginBottom:"10px"}}>
+                <Stack spacing="7px" direction="row">
+                    <TextField
                         type="text"
                         variant='outlined'
                         color='secondary'
@@ -71,6 +76,20 @@ function Settings() {
                         fullWidth
                         required sx={{mb: 4}}
                     />
+                    <TextField
+                        type="text"
+                        variant='outlined'
+                        color='secondary'
+                        label="Home Zip"
+                        onChange={e => {
+                            if(/^\d+$/.test(e.target.value) && e.target.value.length <= 5) setHomeAddressZip(e.target.value)
+                        }}
+                        value={homeAddressZip}
+                        fullWidth
+                        required sx={{mb: 4}}
+                    />
+                </Stack>
+                <Stack spacing="7px" direction="row">
                     <TextField
                         type="text"
                         variant='outlined'
@@ -85,12 +104,42 @@ function Settings() {
                         type="text"
                         variant='outlined'
                         color='secondary'
+                        label="Work Zip"
+                        onChange={e => {
+                            if(/^\d+$/.test(e.target.value) && e.target.value.length <= 5) setWorkAddressZip(e.target.value)
+                        }}
+                        value={workAddressZip}
+                        fullWidth
+                        required sx={{mb: 4}}
+                    />
+                </Stack>
+
+                <Stack spacing="7px" direction="row">
+                    <TextField
+                        type="text"
+                        variant='outlined'
+                        color='secondary'
                         label="Other"
                         onChange={e => setOtherAddress(e.target.value)}
                         value={otherAddress}
                         fullWidth
                         sx={{mb: 4}}
                     />
+                    <TextField
+                        type="text"
+                        variant='outlined'
+                        color='secondary'
+                        label="Other Zip"
+                        onChange={e => {
+                            if(/^\d+$/.test(e.target.value) && e.target.value.length <= 5) setOtherAddressZip(e.target.value)
+                        }}
+                        value={otherAddressZip}
+                        fullWidth
+                        sx={{mb: 4}}
+                    />
+                </Stack>
+                </Stack>
+
                 
                 <Button variant="outlined" color="secondary" type="submit">Save</Button>
             </form>
